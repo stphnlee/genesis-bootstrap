84080c95f488eeabf9e7bddc6d898ea7c29a6462
Stephen Lee <stphnlee@gmail.com>
2013-10-15 11:53:34 -0400

    imported minimum pro theme files; modified functions to remove site tag line

--- functions.php
+++ functions.php
@@ -2,22 +2,13 @@
 //* Start the engine
 include_once( get_template_directory() . '/lib/init.php' );
 
+//* Set Localization (do not remove)
+load_child_theme_textdomain( 'minimum', apply_filters( 'child_theme_textdomain', get_stylesheet_directory() . '/languages', 'minimum' ) );
+
 //* Child theme (do not remove)
-define( 'CHILD_THEME_NAME', 'Genesis Bootstrap Theme' );
-define( 'CHILD_THEME_URL', 'http://www.avidnetizen.com/' );
-define( 'CHILD_THEME_VERSION', '2.0.1' );
-
-//* Enqueue Lato Google font
-add_action( 'wp_enqueue_scripts', 'genesis_bootstrap_google_fonts' );
-function genesis_bootstrap_google_fonts() {
-	wp_enqueue_style( 'google-font-lato', '//fonts.googleapis.com/css?family=Lato:300,700', array(), CHILD_THEME_VERSION );
-}
-
-add_action( 'wp_enqueue_scripts', 'genesis_bootstrap_style' );
-function genesis_bootstrap_style() {
-	wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/bootstrap/css/bootstrap.css' );
-	wp_enqueue_style( 'bootstrap-responsive', get_stylesheet_directory_uri() . '/bootstrap/css/bootstrap-responsive.css' );
-}
+define( 'CHILD_THEME_NAME', __( 'Minimum Pro Theme', 'minimum' ) );
+define( 'CHILD_THEME_URL', 'http://my.studiopress.com/themes/minimum/' );
+define( 'CHILD_THEME_VERSION', '3.0' );
 
 //* Add HTML5 markup structure
 add_theme_support( 'html5' );
@@ -25,17 +16,167 @@ add_theme_support( 'html5' );
 //* Add viewport meta tag for mobile browsers
 add_theme_support( 'genesis-responsive-viewport' );
 
+//* Enqueue scripts
+add_action( 'wp_enqueue_scripts', 'minimum_enqueue_scripts' );
+function minimum_enqueue_scripts() {
+
+	wp_enqueue_script( 'minimum-responsive-menu', get_bloginfo( 'stylesheet_directory' ) . '/js/responsive-menu.js', array( 'jquery' ), '1.0.0' );
+	wp_enqueue_style( 'minimum-google-fonts', '//fonts.googleapis.com/css?family=Roboto:300,400|Roboto+Slab:300,400', array(), CHILD_THEME_VERSION );
+
+}
+
+//* Add new image sizes
+add_image_size( 'portfolio', 540, 340, TRUE );
+
 //* Add support for custom background
-add_theme_support( 'custom-background' );
+add_theme_support( 'custom-background', array( 'wp-head-callback' => '__return_false' ) );
+
+//* Add support for custom header
+add_theme_support( 'custom-header', array(
+	'width'           => 320,
+	'height'          => 60,
+	'header-selector' => '.site-title a',
+	'header-text'     => false
+) );
+
+//* Add support for structural wraps
+add_theme_support( 'genesis-structural-wraps', array(
+	'header',
+	/*'site-tagline',*/
+	'nav',
+	'subnav',
+	'home-featured',
+	'site-inner',
+	'footer-widgets',
+	'footer'
+) );
 
 //* Add support for 3-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 3 );
 
-/** Bootstrap Customizations */
-/** Move primary nav menu 
-remove_action( 'genesis_after_header', 'genesis_do_nav' );
-add_action( 'genesis_before_header', 'genesis_do_nav' );*/
+//* Unregister layout settings
+genesis_unregister_layout( 'content-sidebar-sidebar' );
+genesis_unregister_layout( 'sidebar-content-sidebar' );
+genesis_unregister_layout( 'sidebar-sidebar-content' );
 
-/** Remove Title & Description */
-remove_action( 'genesis_site_title', 'genesis_seo_site_title' );
+//* Unregister secondary sidebar 
+unregister_sidebar( 'sidebar-alt' );
+
+//* Create portfolio custom post type
+add_action( 'init', 'minimum_portfolio_post_type' );
+function minimum_portfolio_post_type() {
+
+	register_post_type( 'portfolio',
+		array(
+			'labels' => array(
+				'name'          => __( 'Portfolio', 'minimum' ),
+				'singular_name' => __( 'Portfolio', 'minimum' ),
+			),
+			'exclude_from_search' => true,
+			'has_archive'         => true,
+			'hierarchical'        => true,
+			'menu_icon'           => get_stylesheet_directory_uri() . '/images/icons/portfolio.png',
+			'public'              => true,
+			'rewrite'             => array( 'slug' => 'portfolio' ),
+			'supports'            => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'trackbacks', 'custom-fields', 'comments', 'revisions', 'page-attributes', 'genesis-seo' ),
+		)
+	);
+	
+}
+
+//* Remove site description
 remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
+
+//* Reposition the primary navigation menu
+remove_action( 'genesis_after_header', 'genesis_do_nav' );
+//*add_action( 'genesis_after_header', 'genesis_do_nav', 15 );
+
+//* Reposition the secondary navigation menu
+remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+//*add_action( 'genesis_footer', 'genesis_do_subnav', 7 );
+
+//* Reduce the secondary navigation menu to one level depth
+add_filter( 'wp_nav_menu_args', 'minimum_secondary_menu_args' );
+function minimum_secondary_menu_args( $args ){
+
+	if( 'secondary' != $args['theme_location'] )
+	return $args;
+
+	$args['depth'] = 1;
+	return $args;
+
+}
+
+/* Add the site tagline section
+add_action( 'genesis_after_header', 'minimum_site_tagline' );
+function minimum_site_tagline() {
+
+	printf( '<div %s>', genesis_attr( 'site-tagline' ) );
+	genesis_structural_wrap( 'site-tagline' );
+
+		printf( '<div %s>', genesis_attr( 'site-tagline-left' ) );
+		printf( '<p %s>%s</p>', genesis_attr( 'site-description' ), esc_html( get_bloginfo( 'description' ) ) );
+		echo '</div>';
+	
+		printf( '<div %s>', genesis_attr( 'site-tagline-right' ) );
+		genesis_widget_area( 'site-tagline-right' );
+		echo '</div>';
+
+	genesis_structural_wrap( 'site-tagline', 'close' );
+	echo '</div>';
+
+}*/
+
+//* Modify the size of the Gravatar in the author box
+add_filter( 'genesis_author_box_gravatar_size', 'minimum_author_box_gravatar' );
+function minimum_author_box_gravatar( $size ) {
+
+	return 144;
+
+}
+
+//* Modify the size of the Gravatar in the entry comments
+add_filter( 'genesis_comment_list_args', 'minimum_comments_gravatar' );
+function minimum_comments_gravatar( $args ) {
+
+	$args['avatar_size'] = 96;
+	return $args;
+
+}
+
+//* Change the number of portfolio items to be displayed (props Bill Erickson)
+add_action( 'pre_get_posts', 'minimum_portfolio_items' );
+function minimum_portfolio_items( $query ) {
+
+	if ( $query->is_main_query() && !is_admin() && is_post_type_archive( 'portfolio' ) ) {
+		$query->set( 'posts_per_page', '6' );
+	}
+
+}
+
+/* Register widget areas
+genesis_register_sidebar( array(
+	'id'          => 'site-tagline-right',
+	'name'        => __( 'Site Tagline Right', 'minimum' ),
+	'description' => __( 'This is the site tagline right section.', 'minimum' ),
+) );*/
+genesis_register_sidebar( array(
+	'id'          => 'home-featured-1',
+	'name'        => __( 'Home Featured 1', 'minimum' ),
+	'description' => __( 'This is the home featured 1 section.', 'minimum' ),
+) );
+genesis_register_sidebar( array(
+	'id'          => 'home-featured-2',
+	'name'        => __( 'Home Featured 2', 'minimum' ),
+	'description' => __( 'This is the home featured 2 section.', 'minimum' ),
+) );
+genesis_register_sidebar( array(
+	'id'          => 'home-featured-3',
+	'name'        => __( 'Home Featured 3', 'minimum' ),
+	'description' => __( 'This is the home featured 3 section.', 'minimum' ),
+) );
+genesis_register_sidebar( array(
+	'id'          => 'home-featured-4',
+	'name'        => __( 'Home Featured 4', 'minimum' ),
+	'description' => __( 'This is the home featured 4 section.', 'minimum' ),
+) );
