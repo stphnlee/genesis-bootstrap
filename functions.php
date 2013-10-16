e501a50c9864f5c6bcd986d1500fc26f61f5bf40
Stephen Lee <stphnlee@gmail.com>
2013-10-16 10:13:41 -0400

    starting from scratch; loaded fresh Genesis Sample child theme files and fresh Bootstrap files. Updated theme info in style.css.

--- functions.php
+++ functions.php
@@ -3,20 +3,14 @@
 include_once( get_template_directory() . '/lib/init.php' );
 
 //* Child theme (do not remove)
-define( 'CHILD_THEME_NAME', 'Genesis Bootstrap Theme' );
-define( 'CHILD_THEME_URL', 'http://www.avidnetizen.com/' );
+define( 'CHILD_THEME_NAME', 'Genesis Sample Theme' );
+define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
 define( 'CHILD_THEME_VERSION', '2.0.1' );
 
 //* Enqueue Lato Google font
-add_action( 'wp_enqueue_scripts', 'genesis_bootstrap_google_fonts' );
-function genesis_bootstrap_google_fonts() {
+add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
+function genesis_sample_google_fonts() {
 	wp_enqueue_style( 'google-font-lato', '//fonts.googleapis.com/css?family=Lato:300,700', array(), CHILD_THEME_VERSION );
-}
-
-add_action( 'wp_enqueue_scripts', 'genesis_bootstrap_style' );
-function genesis_bootstrap_style() {
-	wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/bootstrap/css/bootstrap.css' );
-	wp_enqueue_style( 'bootstrap-responsive', get_stylesheet_directory_uri() . '/bootstrap/css/bootstrap-responsive.css' );
 }
 
 //* Add HTML5 markup structure
@@ -30,12 +24,3 @@ add_theme_support( 'custom-background' );
 
 //* Add support for 3-column footer widgets
 add_theme_support( 'genesis-footer-widgets', 3 );
-
-/** Bootstrap Customizations */
-/** Move primary nav menu */
-remove_action( 'genesis_after_header', 'genesis_do_nav' );
-add_action( 'genesis_before_header', 'genesis_do_nav' );
-
-/** Remove Title & Description 
-remove_action( 'genesis_site_title', 'genesis_seo_site_title' );*/
-remove_action( 'genesis_site_description', 'genesis_seo_site_description' );
